1baace5088db93cddfeba4177104454c64eb6624
Sam Cantu <samantha.cantu@my.utsa.edu>
2024-03-29 00:56:15 -0500

    Player, weapon rotation fixed

--- The Teleporter/Assets/Scripts/PlayerContoller.cs
+++ The Teleporter/Assets/Scripts/PlayerContoller.cs
@@ -28,7 +28,7 @@ public class PlayerContoller : MonoBehaviour
         }
 
         moveDirection = new Vector2(moveX, moveY).normalized;
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 
     }
@@ -37,9 +37,9 @@ public class PlayerContoller : MonoBehaviour
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
 
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimAngle;
+        //Vector2 aimDirection = mousePosition - rb.position;
+        //float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        //rb.rotation = aimAngle;
 
     }
 
--- The Teleporter/Assets/Scripts/Weapon.cs
+++ The Teleporter/Assets/Scripts/Weapon.cs
@@ -8,9 +8,19 @@ public class Weapon : MonoBehaviour
     public Transform firePoint;
     public float fireForce = 20f;
 
+    void Update()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+    }
+
     public void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
     }
+
+
 }
