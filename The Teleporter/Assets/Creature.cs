83886646e3a4d648c2fa4c8a8a2159262c897c30
Sam Cantu <samantha.cantu@my.utsa.edu>
2024-05-07 12:38:08 -0500

    CreatureAI Scripts + Pathfinding Library import

---
+++ The Teleporter/Assets/Creature.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    public float speed = 2.5f;
+    //public AnimationStateCHanger asc;
+    public bool platformingCreature = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector3 offset){
+        if(offset != Vector3.zero){
+            offset.Normalize();
+
+
+            Vector3 vel = offset *= speed;
+            if(platformingCreature){
+                rb.velocity = new Vector2(vel.x,rb.velocity.y);
+
+            }else{
+                rb.velocity = vel;
+            }
+
+            //asc.changeAnimationState("Walking");
+            if(offset.x<0){
+                spriteRenderer.flipX = true;
+                
+            }else{
+                spriteRenderer.flipX = false;
+            }
+        }else{
+            Stop();
+        }
+    }
+
+    public void Stop(){
+        //return;
+        if(platformingCreature){
+            rb.velocity = new Vector2(0,rb.velocity.y);
+
+        }else{
+            rb.velocity = Vector3.zero;
+        }
+        //asc.ChangingAnimationState("Idle");
+    }
+
+    public void MoveToward(Vector3 position){
+        Move(position - transform.position);
+    }
+
+    
+}
---
+++ The Teleporter/Assets/Scripts/CreatureAIHugState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureAIHugState : CreatureAIState
+{
+
+    public CreatureAIHugState(CreatureAI creatureAI) : base(creatureAI){}
+
+
+    public override void BeginState()
+    {
+        creatureAI.SetColor(Color.red);
+    }
+
+    public override void UpdateState()
+    {
+        if(creatureAI.GetTarget() != null){
+            creatureAI.myCreature.MoveToward(creatureAI.GetTarget().transform.position);
+        }else{
+            //creatureAI.ChangeState(creatureAI.investigateState);
+            creatureAI.ChangeState(creatureAI.patrolState);
+        }
+
+    }
+}
