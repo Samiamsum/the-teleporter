da962838bd26db5fc5ac6ed4c8e08772af75fabf
Sam Cantu <samantha.cantu@my.utsa.edu>
2024-05-07 23:44:05 -0500

    Teleporter movement

---
+++ The Teleporter/Assets/Scripts/TheTeleporter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTeleporter : MonoBehaviour
+{
+
+    public float minX = -5f; // Minimum X position
+    public float maxX = 5f; // Maximum X position
+    public float minY = -5f; // Minimum Y position
+    public float maxY = 5f; // Maximum Y position
+    public float teleportInterval = 5f; // Time interval between teleportations
+    protected float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if(timer > 4.5f){
+            timer = 0;
+
+            Teleport();
+        }
+        
+    }
+
+    public void Teleport(){
+        // Generate random X and Y positions within the specified boundaries
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        // Set the position of the enemy to the randomly generated position
+        transform.position = new Vector2(randomX, randomY);
+    }
+
+}
