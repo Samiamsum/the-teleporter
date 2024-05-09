c527d7f6f99be08653ad12fe05e3b9c7f92b3a4d
Sam Cantu <samantha.cantu@my.utsa.edu>
2024-05-08 23:49:34 -0500

    collisions work + healthUI

---
+++ The Teleporter/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    public int health;
+    public int maxHealth;
+
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Image[] hearts;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
